502d98a89c10f338e07a07b7aff6b9c0e25f40df
kndig <rebecca.kuendig@edubs.ch>
2018-08-27 15:48:22 +0200

    Projekt

--- Projekt.py
+++ Projekt.py
@@ -14,33 +14,77 @@ from random import randint
 
 ####C:\Users\Rebecca\Downloads\Bilder\Orb.gif
 
+
+class collider:
+    objects = []
+    def add(self, obj):
+        self.objects.append(obj)
+        return
+    
+    def check(self, p_obj):
+        colliders = []
+        p_pos = p_obj.getPos()
+        left = p_pos[0]-p_obj.half_size[0]
+        right = p_pos[0]+p_obj.half_size[0]
+        top = p_pos[1]-p_obj.half_size[1]
+        bottom = p_pos[1]+p_obj.half_size[1]
+        for idx, obj in enumerate(self.objects):
+            obj_pos = obj.getPos()
+            left2 = obj_pos[0]-obj.half_size[0]
+            right2 = obj_pos[0]+obj.half_size[0]
+            top2 = obj_pos[1]-obj.half_size[1]
+            bottom2 = obj_pos[1]+obj.half_size[1]
+            if left <= right2 and right >= left2 and top <= bottom2 and bottom >= top2:
+                colliders.append(obj)
+        return colliders
+
+m_collider = collider()
+
+
+
 class Zombie(Actor):
     def __init__(self, path):
         Actor.__init__(self, path)
+        colliders = m_collider.check(self)
+        len_col = len(colliders)
+        if len_col > 1:
+            print("ERROR")
         
     def act(self):
         self.move()
-        #direction = Zombie(Human.getX(), Human.getY())
-        #Zombie.setHeading(Zombie.towards(Human))
-        #Zombie.forward(10)
+    
+    def getPos(self):
+        return [self.getX(), self.getY()]
+
         
         
         
 class Human(Actor):
     def __init__(self):
         Actor.__init__(self,"C:\Users\Rebecca\Downloads\Bilder\guywithgun.gif")
+        self.half_size[0] = self.getWidth(0)/2
+        self.half_size[1] = self.getHeight(0)/2
+    
+    half_size = [0,0]
+    def getPos(self):
+        return [self.getX(), self.getY()]
         
 class Bullet(Actor):
     def __init__(self):
         Actor.__init__(self, "C:\Users\Rebecca\Downloads\Bilder\Orb.gif" )
-        self.setCollisionCircle(Point(0, 0), 10)
+        half_size = [0,0]
+        colliders = m_collider.check(self)
+        len_col = len(colliders)
+        if len_col > 1:
+            print("ERROR")
         
     def act(self):
         self.move()
     
-    def collide(self, zombie):
-        self.clear()
-        return 0
+    def getPos(self):
+        return self.pos
+    
+
 
 
 def initZombies():
@@ -49,8 +93,10 @@ def initZombies():
             zombie = Zombie("C:\Users\Rebecca\Downloads\Bilder\zombie" + str(i) + ".jpg")
             Y = randint(0, 600)
             addActor(zombie, Location(800, Y), 180)
+            m_collider.add(zombie)
         doPause()
-        
+
+
             
     
 def onKeyRepeated(keyCode):
@@ -64,13 +110,13 @@ def onKeyRepeated(keyCode):
         human.setY(human.getY() + 5)
     elif keyCode == 32:#shoot
         bullet = Bullet()
+        m_collider.add(bullet)
         addActor(bullet, Location(human.getX(), human.getY()), 0)
             
         
 makeGameGrid(800, 600, 1, None, "sprites/lane.gif", False, keyRepeated = onKeyRepeated)
 setSimulationPeriod(50)
-initZombies()
-#Zombie.addCollisionActor(Bullet)
+#initZombies()
 human = Human()
 addActor(human, Location(0, 300), 0)
 show()
